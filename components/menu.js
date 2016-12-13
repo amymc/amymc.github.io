6b14732d8f1e34985119a7b8264b8a841c993c69
amymc <amy.mccarthy2@mail.dcu.ie>
2016-12-13 22:10:23 +0100

    add new run icon

--- components/menu.js
+++ components/menu.js
@@ -11,13 +11,13 @@ class Menu extends React.Component {
   render() {
     return (
       <ul className='menu'>
-        <li className='menu__item' onClick={this.props.onClick}>
-          <img className='menu__item-icon' src='../assets/os-icons/run-icon.png' alt='run icon'/>
-          Run
-        </li>
         <li className='menu__item'>
           <img className='menu__item-icon' src='../assets/os-icons/run-icon.png' alt='documents icon'/>
           Documents
+        </li>
+        <li className='menu__item' onClick={this.props.onClick}>
+          <img className='menu__item-icon' src='../assets/os-icons/run-icon.png' alt='run icon'/>
+          Run
         </li>
       </ul>
     );
