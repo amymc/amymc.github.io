c2620e0b1e28f12a5b698a1d3b624ebc9112c71c
amymc <amy.mccarthy2@mail.dcu.ie>
2016-12-14 16:28:46 +0100

    add settings to menu

--- components/menu.js
+++ components/menu.js
@@ -15,6 +15,10 @@ class Menu extends React.Component {
           <img className='menu__item-icon' src='../assets/os-icons/documents-icon.png' alt='documents icon'/>
           <span className='menu__item-title'> Documents &#x25b6; </span>
         </li>
+        <li className='menu__item'>
+          <img className='menu__item-icon' src='../assets/os-icons/settings-icon.png' alt='settings icon'/>
+          <span className='menu__item-title'> Settings &#x25b6; </span>
+        </li>
         <li className='menu__item' onClick={this.props.onClick}>
           <img className='menu__item-icon' src='../assets/os-icons/run-icon.png' alt='run icon'/>
           <span className='menu__item-title'> Run </span>
