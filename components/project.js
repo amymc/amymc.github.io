2578f5fc0d3bb773ba91ec6ddb9985e5cf75f03a
amymc <amy.mccarthy2@mail.dcu.ie>
2017-01-06 19:51:14 +0000

    make projects draggable

--- components/project.js
+++ components/project.js
@@ -1,34 +1,67 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Draggable, {DraggableCore} from 'react-draggable';
 import CloseButton from './closebutton';
 import '../styles/project.scss';
 
-const Project = (props) => {
+class Project extends React.Component {
+  constructor(props) {
+    super(props);
 
+    // this.state = {
+    //   hover: false
+    // };
+
+    this.state ={
+      activeDrags: 0
+    };
+
+    this.onStart = this.onStart.bind(this);
+    this.onStop = this.onStop.bind(this);
+  }
+
+  onStart() {
+    this.setState({activeDrags: ++this.state.activeDrags});
+  }
+
+  onStop() {
+    this.setState({activeDrags: --this.state.activeDrags});
+  }
+
+  render() {
 //  render() {
-  const project = props;
+  const project = this.props;
 
-  return (
-    <div className='project'>
-      <div className='project__header'>
-        <h1 className='project__title'>
-          {project.title}
-        </h1>
-        <CloseButton onClick={props.onClick}/>
-      </div>
-      <div className='project__inner-wrapper'>
-        <div className='project__image-wrapper'>
-          { project.image_urls.map((url, index) => {
-            return <img key={index} className='project__image' alt={project.title} src={`../assets/${url}`} />
-          })}
+    return (
+      <Draggable
+          handle=".handle"
+          position={null}
+          zIndex={100}
+          onStart={this.onStart}
+          onDrag={this.onDrag}
+          onStop={this.onStop}>
+        <div className='project'>
+          <div className='project__header handle'>
+            <h1 className='project__title'>
+              {project.title}
+            </h1>
+            <CloseButton onClick={this.props.onClick}/>
+          </div>
+          <div className='project__inner-wrapper'>
+            <div className='project__image-wrapper'>
+              { project.image_urls.map((url, index) => {
+                return <img key={index} className='project__image' alt={project.title} src={`../assets/${url}`} />
+              })}
+            </div>
+            <div className='project__info-wrapper'>
+              {project.description}
+              <a href={project.company_url} target='_blank'>Link</a>
+            </div>
+          </div>
         </div>
-        <div className='project__info-wrapper'>
-          {project.description}
-          <a href={project.company_url} target='_blank'>Link</a>
-        </div>
-      </div>
-    </div>
-  )
+      </Draggable>
+    )
+  }
 }
 
 export default Project;
