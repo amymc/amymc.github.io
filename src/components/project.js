e99ca40be36bb28fe94aa80bea5bf1ddc97920e6
amymc <amy.mccarthy2@mail.dcu.ie>
2017-02-01 00:58:17 +0000

    make project stateless component

--- src/components/project.js
+++ src/components/project.js
@@ -4,30 +4,24 @@ import Draggable, {DraggableCore} from 'react-draggable';
 import CloseButton from './closebutton';
 import '../styles/components/project.scss';
 
-class Project extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const Project = (props) => {
+  const item = this.props;
 
-  render() {
-    const item = this.props;
-
-    return (
-      <div className='project'>
-        <div className='project__inner-wrapper'>
-          <div className='project__image-wrapper'>
-            { item.image_urls.map((url, index) => {
-              return <img key={index} className='project__image' alt={item.title} src={`../src/assets/${url}`} />
-            })}
-          </div>
-          <div className='project__info-wrapper'>
-            {item.description}
-            <a href={item.company_url} target='_blank'>Link</a>
-          </div>
+  return (
+    <div className='project'>
+      <div className='project__inner-wrapper'>
+        <div className='project__image-wrapper'>
+          { item.image_urls.map((url, index) => {
+            return <img key={index} className='project__image' alt={item.title} src={`../src/assets/${url}`} />
+          })}
+        </div>
+        <div className='project__info-wrapper'>
+          {item.description}
+          <a href={item.company_url} target='_blank'>Link</a>
         </div>
       </div>
-    )
-  }
+    </div>
+  );
 }
 
 export default Project;
