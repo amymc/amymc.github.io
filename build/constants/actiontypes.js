6b8a33da91b0ab83974c252ece87f3671e60accf
amymc <amy.mccarthy2@mail.dcu.ie>
2017-08-12 16:09:18 +0100

    remove build folder from repo

--- build/constants/actiontypes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var OPEN_POPUP = exports.OPEN_POPUP = 'OPEN_POPUP';
-var CLOSE_POPUP = exports.CLOSE_POPUP = 'CLOSE_POPUP';
-var OPEN_PROJECT = exports.OPEN_PROJECT = 'OPEN_PROJECT';
-var CLOSE_PROJECT = exports.CLOSE_PROJECT = 'CLOSE_PROJECT';
