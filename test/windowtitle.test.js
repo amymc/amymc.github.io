12d3102705086f6972ace16fdcd62383848ed08c
McCarthy <mccarthya@amymc.local>
2017-08-30 19:56:48 +0100

    windowtitle tests, wip

--- test/windowtitle.test.js
+++ test/windowtitle.test.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
 import WindowTitle from '../src/components/windowtitle';
+import CloseButton from '../src/components/closebutton';
 
 describe("Windowtitle", function() {
-  beforeEach(() => {
-    props.title = "Fake title";
-  });
+  // beforeEach(() => {
+  //   props.title = "Fake title";
+  // });
 
-  it("always renders a div", () => {
-    const divs = WindowTitle().find("div");
-    expect(divs.length).toBeGreaterThan(0);
+  //console.log('test', WindowTitle);
+
+  it("contains a close button", () => {
+    const wrapper = shallow(<WindowTitle />);
+    expect(wrapper.find(CloseButton).length).to.equal(1);
+    //expect(divs.length).toBeGreaterThan(0);
   });
 
   it("displays the title", function() {
@@ -21,3 +25,11 @@ describe("Windowtitle", function() {
     
   });
 });
+
+
+// describe('<MyInputComponent/>', () => {
+//     it('renders without exploding', () => {
+//         const wrapper = shallow(<MyInputComponent />);
+//         expect(wrapper.find(MyInputComponent).length).to.equal(1);
+//     });
+// });
