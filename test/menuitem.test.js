99fdc9f45555dd27c9857c532beafa63953c7d6b
amymc <amy.mccarthy2@mail.dcu.ie>
2017-10-21 17:35:48 +0100

    fix import error

--- test/menuitem.test.js
+++ test/menuitem.test.js
@@ -9,18 +9,16 @@ describe("MenuItem", function() {
   beforeEach(() => {
     this.callback = sinon.spy();
     this.wrapper = shallow(<MenuItem item={data.item} onClick={this.callback} />);
-    // this.contactWrapper = shallow(<Popup {...data.contactItem} onClick={this.callback} />);
-    // this.projectsWrapper = shallow(<Popup {...data.projectsItem} onClick={this.callback} sideProjects={data.sideProjects}/>);
   });
 
   it("renders self", () => {
     expect(this.wrapper.find('.menu-item').length).to.equal(1);
   });
 
-  // it("renders contact", () => {
-  //   expect(this.contactWrapper.find('.popup__inner-wrapper--contact').length).to.equal(1);
-  //   expect(this.contactWrapper.find('.popup__link').length).to.equal(data.contactItem.links.length);
-  // });
+  it("renders submenu on hover if available", () => {
+    this.wrapper.find('button').simulate('onmouseover');
+
+  });
 
   // it("renders side projects", () => {
   //   expect(this.projectsWrapper.find('.popup__projects-wrapper').length).to.equal(1);
