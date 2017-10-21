e22a1769c4d996eed86710faa93c5796ed1bd7c0
amymc <amy.mccarthy2@mail.dcu.ie>
2017-10-21 17:22:16 +0100

    fix import error

--- test/menuitem.test.js
+++ test/menuitem.test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { expect, assert } from 'chai';
 import sinon from 'sinon';
 import { shallow } from 'enzyme';
-import { MenuItem } from '../src/components/menuitem';
+import MenuItem from '../src/components/menuitem';
 import * as data from './test-data/menuitem.data.js';
 
 describe("MenuItem", function() {
   beforeEach(() => {
     this.callback = sinon.spy();
-    this.wrapper = shallow(<MenuItem {...data.defaultItem} item={data.item} onClick={this.callback} />);
+    this.wrapper = shallow(<MenuItem item={data.item} onClick={this.callback} />);
     // this.contactWrapper = shallow(<Popup {...data.contactItem} onClick={this.callback} />);
     // this.projectsWrapper = shallow(<Popup {...data.projectsItem} onClick={this.callback} sideProjects={data.sideProjects}/>);
   });
--- test/test-data/menuitem.data.js
+++ test/test-data/menuitem.data.js
@@ -1,48 +1,23 @@
-export const item = [
-  {
-    title: "Documents",
-    image_url: "documents-icon.png",
-    onClick: false,
-    onMouseOut: true,
-    onMouseOver: true,
-    isSubmenu: false,
-    popup: null,
-    items: [
-      {
-        title: "Random shit",
-        image_url: "random-icon.png",
-        onClick: true,
-        onMouseOut: false,
-        onMouseOver: false,
-        isSubmenu: true,
-        popup: {
-            title: "Random shit"
-          },
-        items: null
-      }
-    ]
-  },
-  {
-    title: "Settings",
-    image_url: "settings-icon.png",
-    onClick: false,
-    onMouseOut: true,
-    onMouseOver: true,
-    isSubmenu: false,
-    popup: null,
-    items: [
-      {
-        title: "Control Panel",
-        image_url: "controlpanel-icon.png",
-        onClick: true,
-        onMouseOut: false,
-        onMouseOver: false,
-        isSubmenu: true,
-        popup: {
-            title: "Error"
-          },
-        items: null
-      }
-    ]
-  }
-]
+export const item = {
+  title: "Documents",
+  image_url: "documents-icon.png",
+  onClick: false,
+  onMouseOut: true,
+  onMouseOver: true,
+  isSubmenu: false,
+  popup: null,
+  items: [
+    {
+      title: "Random shit",
+      image_url: "random-icon.png",
+      onClick: true,
+      onMouseOut: false,
+      onMouseOver: false,
+      isSubmenu: true,
+      popup: {
+          title: "Random shit"
+        },
+      items: null
+    }
+  ]
+};
