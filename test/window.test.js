b928589515d147c825790e19dee1c24936c5370e
amymc <amy.mccarthy2@mail.dcu.ie>
2017-11-05 14:29:03 +0000

    window tests wip

--- test/window.test.js
+++ test/window.test.js
@@ -1,33 +1,44 @@
 import React from 'react';
 import { assert, expect } from 'chai';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import sinon from 'sinon';
 import Draggable from 'react-draggable';
 import { Resizable } from 'react-resizable';
 import { Window } from '../src/components/window';
 import * as projectData from './test-data/project.data.js';
+import * as popupData from './test-data/popup.data.js';
 
 describe("Window", function() {
   beforeEach(() => {
     this.callback = sinon.spy();
-    this.wrapper = shallow(<Window item={projectData.item} onMouseDown={this.callback(projectData.item.title)} />);
+    this.projectWrapper = shallow(<Window item={projectData.item} isProject={true} onMouseDown={this.callback(projectData.item.title)} />);
+    this.popupWrapper = shallow(<Window item={popupData.item} isProject={false} />);
   });
 
   it("renders a div", () => {
-    expect(this.wrapper.find('div').length).to.equal(1);
+    expect(this.projectWrapper.find('div').length).to.equal(1);
   });
 
   it("only renders the draggable component on desktop", () => {
-    this.wrapper.instance().checkWindowWidth();
+    this.projectWrapper.instance().checkWindowWidth();
     if (window.innerWidth > 540) {
-      expect(this.wrapper.find('Draggable').length).to.equal(1);
+      expect(this.projectWrapper.find('Draggable').length).to.equal(1);
     } else {
-      expect(this.wrapper.find('Draggable').length).to.equal(0);
+      expect(this.projectWrapper.find('Draggable').length).to.equal(0);
     }
   });
 
   it("calls the onMouseDown prop on mouse down", () => {
-    this.wrapper.simulate('mouseDown');
+    this.projectWrapper.simulate('mouseDown');
     assert(this.callback.calledOnce);
+  });
+
+  it("displays correct layout", () => {
+    assert(expect(this.projectWrapper.find('.window--project').length).to.equal(1));
+    assert(expect(this.popupWrapper.find('.window--popup').length).to.equal(1));
+  });
+
+  it("checks width on resize", () => {
+
   });
 });
